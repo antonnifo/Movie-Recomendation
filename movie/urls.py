1e93bb65914a813edc0fbfe256fd7718da03e973
antonnifo <antonnifo@live.com>
2023-03-14 15:16:51 +0300

    change home url to function based

--- movie/urls.py
+++ movie/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from django.views.generic import TemplateView
+# from django.views.generic import TemplateView
 from . import views
 
 app_name = 'movie'
 
 urlpatterns = [
-     path('', TemplateView.as_view(template_name="site/index.html"), name='home'),
+     # path('', TemplateView.as_view(template_name="site/index.html"), name='home'),
+     path('', views.index, name='home'),
      path('results/', views.search_results, name='search'),
 ]
