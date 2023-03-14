9f991b4198320dfb752edd77feae55781143414a
antonnifo <antonnifo@live.com>
2023-03-14 15:15:00 +0300

    add url path for human app

--- Engine/urls.py
+++ Engine/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
      path('', include('movie.urls', namespace='movie')),
-    path('admin/', admin.site.urls),
+     path('admin/', admin.site.urls),
+     path('account/', include('human.urls')),
+     path('social-auth/',
+            include('social_django.urls', namespace='social')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                            document_root=settings.MEDIA_ROOT)
+
