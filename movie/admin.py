bf519321826cc289a6f29e41a1d55a97c75dc6d1
antonnifo <antonnifo@live.com>
2023-03-15 13:16:26 +0300

    register movie model in admin panel

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from  .models import Movie
 
-# Register your models here.
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display  = ('title', 'genres', 'year', 'actors', 'director', 'created', 'updated')
+    search_fields = ('title', 'actors', 'director')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy      = 'updated'
+    list_per_page       = 10    
